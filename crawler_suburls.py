ad1dee1be30278d0f38f52970a08c9d6edc350a7
M ALI RAZA <itsaaliraza@gmail.com>
2025-03-08 23:57:32 +0500

    sub crawler

--- crawler_suburls.py
+++ crawler_suburls.py
@@ -1,71 +1,3 @@
-# from botasaurus.browser import browser, Driver
-# from botasaurus.soupify import soupify
-# from botasaurus.browser import Wait
-# import time
-# import csv
-# from urllib.parse import urljoin
-#
-# input_file = "output/company_urls.csv"
-# output_file = "output/company_suburls.csv"
-# base_url = "https://www.glassdoor.co.uk"
-# companies_jobs_suburls = []
-# crawler_urls = []
-#
-#
-# def read_url_from_csv():
-#     with open(input_file, mode="r", newline="", encoding='utf-8') as file:
-#         reader = csv.reader(file)
-#         crawler_urls = [row[0] for row in reader if row]
-#         return crawler_urls[1:]
-#
-#
-# def write_to_csv(link):
-#     with open(output_file, mode="a", newline="", encoding="utf-8") as file:
-#         writer = csv.writer(file)
-#         writer.writerow([link])
-#
-#
-#
-#
-#
-# @browser
-# def crawl_suburls(driver: Driver, link):
-#     pages = 1
-#     try:
-#         driver.google_get(link)
-#         while True:
-#             time.sleep(2)
-#             page_numbers = driver.select_all('p.pagination_PageNumberText__zy_hr')
-#             total_pages = page_numbers[-1].text
-#
-#             if pages > int(total_pages):
-#                 break
-#             sub_links = driver.select_all('a.salarylist_job-title-link__MXnPX')
-#             for sub_link in sub_links:
-#                 link = sub_link.get_attribute('href')
-#                 if link:
-#                     full_url = urljoin(base_url, link)
-#                     print(full_url)
-#                     companies_jobs_suburls.append(full_url)
-#                     write_to_csv(full_url)
-#             element = driver.select("button[data-test='next-page']")
-#             if element:
-#                 element.scroll_into_view()
-#                 time.sleep(1)
-#                 element.click()
-#                 time.sleep(15)
-#             else:
-#                 print("Element not found.")
-#             pages = pages + 1
-#     except Exception as e:
-#         print(f"EXCEPTION {str(e)}")
-#
-#
-# urls = read_url_from_csv()
-# print(urls)
-# crawl_suburls(["https://www.glassdoor.co.uk/Salary/Bon-Soiree-Salaries-E10196089.htm",
-#                "https://www.glassdoor.co.uk/Salary/TravelConnect-Salaries-E1189862.htm"])
-
 
 from botasaurus.browser import browser, Driver
 from botasaurus.soupify import soupify
@@ -76,7 +8,6 @@ import csv
 import threading
 import logging
 
-
 log_filename = f"logs/crawler_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
 logging.basicConfig(
     level=logging.INFO,
@@ -84,8 +15,6 @@ logging.basicConfig(
     handlers=[logging.FileHandler(log_filename), logging.StreamHandler()],
 )
 logger = logging.getLogger(__name__)
-
-
 
 write_lock = threading.Lock()
 concurrency = 10
@@ -102,10 +31,12 @@ def read_url_from_csv():
         crawler_urls = [row[0] for row in reader if row]
         return crawler_urls[1:]
 
+
 # Initialize CSV file with header
 with open(output_file, mode="w", newline="", encoding="utf-8") as file:
     writer = csv.writer(file)
     writer.writerow(["Salary_URL"])
+
 
 def write_to_csv(link):
     with write_lock:
@@ -117,7 +48,8 @@ def write_to_csv(link):
 url = "https://www.glassdoor.co.uk/Salary/DONE-by-NONE-Salaries-E619738.htm"
 
 
-@browser(parallel=concurrency,max_retry=20,output=None,close_on_crash=True,raise_exception=True,create_error_logs=True)
+@browser(parallel=concurrency, max_retry=20, output=None, close_on_crash=True, raise_exception=True,
+         create_error_logs=True)
 def scrape_heading_task(driver: Driver, link):
     page_number = 1
     try:
@@ -148,17 +80,15 @@ def scrape_heading_task(driver: Driver, link):
             else:
                 print("Element not found.")
             page_number = page_number + 1
-            logger.info(f"Page {page_number} error: Found {len(companies_jobs_suburls)} ")
+            logger.info(f"Scraped {len(companies_jobs_suburls)} job sub-links from {page_number} pages.")
+
     except Exception as e:
         logger.error(f"Page {page_number} error: {str(e)}")
     return companies_jobs_suburls
 
+
 if __name__ == "__main__":
     urls = read_url_from_csv()
     logger.info(f"Start crawling sub links ")
-    result=scrape_heading_task(["https://www.glassdoor.co.uk/Salary/CMG-France-Salaries-E2968945.htm","https://www.glassdoor.co.uk/Salary/TravelConnect-Salaries-E1189862.htm"])
+    result = scrape_heading_task(urls)
     logger.info(f"Crawling complete. Total links collected: {len(result)} ")
-
-
-
-
