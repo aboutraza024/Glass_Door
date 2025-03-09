e614c24dbc14c4f8acb98e875372f8927db0474e
M ALI RAZA <itsaaliraza@gmail.com>
2025-03-10 01:28:59 +0500

    final scrapper code

--- jobs_scrapper.py
+++ jobs_scrapper.py
@@ -2,7 +2,7 @@ import time
 import csv
 from botasaurus.browser import browser, Driver
 
-input_file = "output/company_suburls2.csv"  # Change it to the correct suburls
+input_file = "output/company_suburls.csv"
 output_file = "output/companies_data.csv"
 
 
@@ -25,14 +25,15 @@ with open(output_file, mode="w", newline="", encoding="utf-8") as file:
 
 url = "https://www.glassdoor.co.uk/Salary/Freelance-Freelancer-Salaries-E1130527_D_KO10,20.htm"
 
-
-experience_levels = [
+# List of experience levels to click
+experience_levels = ["All Years of Experience",
     "0-1 Year", "1-3 Years", "4-6 Years",
     "7-9 Years", "10-14 Years", "15+ Years"
 ]
 
 
 def wait_for_element(driver, selector, timeout=5):
+    """Wait for an element to appear on the page."""
     start_time = time.time()
     while time.time() - start_time < timeout:
         element = driver.select(selector)
@@ -43,26 +44,29 @@ def wait_for_element(driver, selector, timeout=5):
 
 
 def open_experience_dropdown(driver):
+    """ Opens the experience filter dropdown. """
     try:
         experience_elements = driver.select_all("div[class*='filter-chip_FilterChip']")
         if len(experience_elements) > 1:
-
+            time.sleep(1.3)
             experience_elements[1].click()
             time.sleep(2)
     except Exception as e:
         print(f"Error opening dropdown: {e}")
 
-# base_pay_text=[]
+base_pay_text=[]
 def write_to_csv(data):
+    """Appends a row to the CSV file."""
     with open(output_file, mode="a", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
         writer.writerow(data)
 
 def scrape_required_info(driver, experience):
-
+    """Scrapes required information and writes to CSV."""
     try:
         companies_data = []  # New list for each row
-        country = "UK"  # Set country manually because we need to only scrape Uk data
+
+        country = "UK"  # Set country manually uk because we only need to find uk data
         companies_data.append(country)
 
         company_name = driver.select("div.employer-header_nameAndRating___HtOS > p")
@@ -78,17 +82,19 @@ def scrape_required_info(driver, experience):
             job_title="N/A"
             companies_data.append(job_title)
 
-        companies_data.append(experience)  # Add years of experience in list
+        companies_data.append(experience)
 
         base_pay_element = driver.select_all('div.hero_PayRange__nKzVj')
         base_pay_text = base_pay_element[0].text if base_pay_element else "N/A"
         companies_data.append(base_pay_text)
 
         average_pay = driver.select('div.hero_AdditionalPayAverage__yS6Cc > span')
+        # print(average_pay.text)
         additional_pay_average = average_pay.text if average_pay else "N/A"
         companies_data.append(additional_pay_average)
 
         range_salary = driver.select("div.hero_AdditionalPayRange__I5R6_ > span")
+        # print(range_salary.text)
         additional_pay_range = range_salary.text if range_salary else "N/A"
         companies_data.append(additional_pay_range)
 
@@ -111,16 +117,18 @@ def scrape_required_info(driver, experience):
     except Exception as e:
         print("Error while scraping data:", e)
 
-@browser()
+@browser(parallel=3)
 def scrape_companies_data(driver: Driver, link):
+
     try:
         driver.google_get(link)
-        time.sleep(3)
+        time.sleep(3)  # Wait for page to load
 
         for exp in experience_levels:
-            open_experience_dropdown(driver)
+            open_experience_dropdown(driver)  # Open dropdown
             time.sleep(1)
 
+            # Wait for button to appear
             button_selector = f"button[aria-label='{exp}']"
             experience_button = wait_for_element(driver, button_selector, timeout=5)
 
@@ -131,12 +139,11 @@ def scrape_companies_data(driver: Driver, link):
             else:
                 print(f"Button for {exp} not found!")
 
-            time.sleep(5)
+            time.sleep(5)  # Wait for data to load
 
     except Exception as e:
         print(f"Error: {e}")
-
-
-scrape_companies_data(url)
+urls=read_url_from_csv()
+scrape_companies_data(urls[10:12])
 
 
